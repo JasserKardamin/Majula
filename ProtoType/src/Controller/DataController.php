cf7440e2f0dfdb697ac9179b1b959d8256812073
ayoub <ayoub.chouat@esprit.tn>
2024-11-08 01:01:50 +0100

    adding sign in + signup (mail verif) + forgot password (mail verif)

--- ProtoType/src/Controller/DataController.php
+++ ProtoType/src/Controller/DataController.php
@@ -4,9 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Data;
 use App\Form\DataType;
-use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use PhpParser\Builder\Method;
 //use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -25,35 +23,14 @@ class DataController extends AbstractController
     }
 
     private function get_data_page() : array {
-
+        {
             return $this->entityManager->createQueryBuilder()
                 ->select('a') 
                 ->from('App\Entity\Data','a')
-                ->orderBy('a.id','DESC')
-                ->setMaxResults(8)
+                ->setMaxResults(10)
                 ->getQuery()
                 ->getResult();
-
-    }
-
-    private function get_courses(string $value) : array {
-            return $this->entityManager->createQueryBuilder()
-                ->select('a') 
-                ->from('App\Entity\Data','a')
-                ->where('a.type = :type')
-                ->setParameter('type',$value) 
-                ->getQuery()
-                ->getResult();
-    }
-
-    private function get_courses_title(string $value): array {
-        return $this->entityManager->createQueryBuilder()
-            ->select('d')
-            ->from('App\Entity\Data', 'd')
-            ->where('d.title LIKE :searchvalue') 
-            ->setParameter('searchvalue', '%' . $value . '%') 
-            ->getQuery()
-            ->getResult();
+        }
     }
     
     #[Route('/data', name: 'app_data')]
@@ -66,57 +43,6 @@ class DataController extends AbstractController
         ]);
     }
 
-    #[Route('/data/add', name: 'add_data')]
-    public function add(Request $request ): Response
-    {
-        $data = new data() ;
-        $form = $this->createForm(Datatype::class,$data);
-        $form->handleRequest($request) ; 
-
-        if($form->isSubmitted() && $form->isValid()) {
-            //$otherentity = $form->getData('') ; 
-            $data->setLikes(0) ; 
-
-            $currecnt_date = new DateTime() ; 
-            $data->setPublicationdate($currecnt_date) ; 
-            $data->setApproved(0) ; 
-            $data->setAction("published") ;
-
-
-            $query = $this->entityManager->createQuery('SELECT u FROM App\Entity\User u ORDER BY u.id ASC');
-            $query->setMaxResults(1);
-            $user =  $query->getOneOrNullResult(); 
-            $data->setUser($user);
-
-
-            $this->entityManager->persist($data);
-            $this->entityManager->flush();
-            
-            return $this->redirectToRoute('app_data');
-        }
-
-        return $this->render('data/add.html.twig', [
-             'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/data/myposts', name: 'show_data_myposts')]
-    public function myposts(): Response
-    {
-        $query = $this->entityManager->createQuery('SELECT u FROM App\Entity\User u ORDER BY u.id ASC');
-        $query->setMaxResults(1);
-        $user =  $query->getOneOrNullResult(); 
-
-        $query = $this->entityManager->createQuery('SELECT d FROM App\Entity\Data d WHERE d.user = :user') ; 
-        $query->setParameter('user',$user) ; 
-        $posts = $query->getResult() ; 
-
-        return $this->render('data/index.html.twig', [
-            'data' => $posts,
-        ]);
-    }
-
-
     #[Route('/data/details/{id}', name: 'show_data_details')]
     public function details(?int $id = null): Response
     {
@@ -126,88 +52,6 @@ class DataController extends AbstractController
             'data' => $data,
         ]);
     }
-
-
-    #[Route('/data/display_all_courses', name: 'display_all_courses')]
-    public function Courses(): Response
-    {
-        $data = $this->get_courses('course') ; 
-
-        return $this->render('data/index.html.twig', [
-            'data' => $data,
-        ]);
-    }
-
-
-    #[Route('/data/display_all_tds', name: 'display_all_tds')]
-    public function Tds(): Response
-    {
-        $data = $this->get_courses('serie') ; 
-
-        return $this->render('data/index.html.twig', [
-            'data' => $data,
-        ]);
-    }
-
-    #[Route('/data/display_all_tests', name: 'display_all_tests')]
-    public function Tests(): Response
-    {
-        $data = $this->get_courses('Test') ; 
-
-        return $this->render('data/index.html.twig', [
-            'data' => $data,
-        ]);
-    }
-
-    #[Route('/data/display_all_Exams', name: 'display_all_exams')]
-    public function Exams(): Response
-    {
-        $data = $this->get_courses('Exam') ; 
-
-        return $this->render('data/index.html.twig', [
-            'data' => $data,
-        ]);
-    }
-
-    #[Route('/data/search', name: 'data_search')]
-    public function search(Request $request ): Response
-    {
-        $data = []; 
-        $search_value = '' ;
-        
-        
-        if ($request->isMethod('POST')) {
-            $search_value = $request->request->get('search_value');
-
-            if($search_value == '' ) {
-                return $this->redirectToRoute('app_data');
-            }
-
-            $data = $this->get_courses_title($search_value);
-        }
-        return $this->render('data/index.html.twig', [
-            'data' => $data,
-            'searchvalue' => $search_value
-
-        ]);
-    }
-
-    #[Route('/data/suggest', name: 'data_suggest')]
-    public function suggest(Request $request): JsonResponse
-    {
-        $query = $request->query->get('query'); 
-        $suggestions = $this->get_courses_title($query); 
-        $result = array_map(fn($item) => ['title' => $item->getTitle()], $suggestions); 
-
-        return new JsonResponse($result); 
-    }
-
-
-
-
-
-
-
 
     /* later : 
     #[Route('/data/{page}', name: 'load_more_data')]
--- ProtoType/src/Form/DataType.php
+++ ProtoType/src/Form/DataType.php
@@ -3,11 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Data;
-use Symfony\Component\Form\Extension\Core\Type\TextType;  
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType; 
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType; 
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,58 +14,19 @@ class DataType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class  , [
-                'attr' => [
-                    'class' => 'input',
-                    'placeholder' => 'Enter the Title'
-                ]
+            ->add('title')
+            ->add('content')
+            ->add('likes')
+            ->add('publicationdate', null, [
+                'widget' => 'single_text',
             ])
-            
-            ->add('content' , FileType::class, [  
-                'attr' => [
-                    'class' => 'file', 
-                ],
-                'label' => 'Upload File',  
-                'mapped' => false,  
-                'required' => false,  
+            ->add('approved')
+            ->add('action')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
             ])
-
-            ->add('description' ,TextareaType::class, [  
-                'attr' => [
-                    'class' => 'input_area', 
-                    'placeholder' => 'Enter the Description', 
-                    'rows' => 5, 
-                    'cols' => 57
-                ]
-            ])
-
-            ->add('class', ChoiceType::class , [
-                'attr' => [
-                    'class' => 'select',
-                    'placeholder' => 'Enter the Title'
-                ],
-                'label' => 'Class', 'choices' => [
-                     '1A' => '1',
-                     '2A' => '2',
-                     '3A' => '3',
-                     '4A' => '4',
-                     '5A' => '5'
-                     ], 
-               ])
-
-            ->add('type', ChoiceType::class , [
-                'attr' => [
-                    'class' => 'select',
-                    'placeholder' => 'Enter the Title'
-                ],
-                'label' => 'Type', 'choices' => [
-                     'Serie' => 'serie',
-                     'Test' => 'test',
-                     'Exam' => 'exam',
-                     'Course' => 'couse'
-                     ], 
-               ]) ;
-        
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
